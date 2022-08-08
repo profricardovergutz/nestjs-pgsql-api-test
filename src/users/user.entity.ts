908976f63c4f4d84c0ab0ef27b915bfb40f0dd36
Ricardo Vergutz <ricardovergutz@gmail.com>
2022-08-07 23:30:50 -0300

    teste desempenho

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -23,21 +23,6 @@ export class User extends BaseEntity {
   @Column({ nullable: false, type: 'varchar', length: 20 })
   role: string;
 
-  @Column({ nullable: false, default: true })
-  status: boolean;
-
-  @Column({ nullable: false })
-  password: string;
-
-  @Column({ nullable: false })
-  salt: string;
-
-  @Column({ nullable: true, type: 'varchar', length: 64 })
-  confirmationToken: string;
-
-  @Column({ nullable: true, type: 'varchar', length: 64 })
-  recoverToken: string;
-
   @CreateDateColumn()
   createdAt: Date;
 
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,18 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUserDto } from './create-user.dto';
 import { ReturnUserDto } from './return-user.dto';
+import { User } from './user.entity';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private userService: UsersService) {}
+
+  @Get()
+  async findAll(): Promise<User[]> {
+    const users = await this.userService.findAll();
+    return users;
+  }
 
   @Post()
   async createAdminUser(
